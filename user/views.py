b481ac3dff37ae0d507597869dd17f79119659a6
20240663-creator <20240663@stud.fci-cu.edu.eg>
2026-04-15 16:14:22 +0200

    updated user view

--- user/views.py
+++ user/views.py
@@ -111,14 +111,14 @@ def view_deposit(request):
             status = 'success',
             transaction_type = 'Deposit',
         )
-
+        user.total_income += new_amount
+        user.save()
         return render(request, 'deposit.html', context)
 
     return render(request, 'deposit.html',{'user':user})
 
 
 def view_logout(request):
-    # Clear the session
     if 'user_id' in request.session:
         del request.session['user_id']
     return redirect('login')
