37451ba9613202e272773566a2711053e186f8c6
Ali-Sa3d-G <as7476980@gmail.com>
2026-05-09 00:59:29 +0300

    Delete user directory

--- user/admin.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing.reduction import register
-from . import models
-
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(models.WalletUser)
-admin.site.register(models.Wallet)
